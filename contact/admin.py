7330ef8a38d33c500835d0bb9ccb9239bc708e99
Andre Oliveira <dev.andrefc@gmail.com>
2023-05-10 16:59:17 -0300

    Adição da função inserir foto no cadastro do contato

--- contact/admin.py
+++ contact/admin.py
@@ -3,4 +3,11 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    pass    
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'email', 
+    ordering = 'id',
+    # list_filter = 'created_date',
+    search_fields = 'id', 'first_name', 'last_name', 'phone', 'email',
+    list_per_page = 10
+    list_max_show_all = 100
+    list_editable = 'first_name',
+    list_display_links = 'last_name','phone','email',
--- contact/models.py
+++ contact/models.py
@@ -10,6 +10,8 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(max_length=500, blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
