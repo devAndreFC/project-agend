b5f4f676c8cf59d50a491d4ee3ba8517356add14
Andre Oliveira <dev.andrefc@gmail.com>
2023-05-11 17:06:04 -0300

    Criação da página de vizualização do contato

--- contact/admin.py
+++ contact/admin.py
@@ -4,13 +4,13 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone', 'email', 
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'email', 'show',
     ordering = 'id',
     # list_filter = 'created_date',
     search_fields = 'id', 'first_name', 'last_name', 'phone', 'email',
     list_per_page = 10
     list_max_show_all = 100
-    # list_editable = 'first_name',
+    list_editable = 'show',
     list_display_links = 'id', 'first_name', 'last_name','phone','email',
     
     
--- contact/urls.py
+++ contact/urls.py
@@ -3,5 +3,7 @@ from contact import views
 
 app_name = 'contact'
 urlpatterns = [
+    path('<int:contact_id>/', views.contact, name='contact'),
     path('', views.index, name='index'),
+
 ]
