5144df3cbf6e65f0506d6df2deb7a1f871b48977
Andre Oliveira <dev.andrefc@gmail.com>
2023-05-12 15:13:01 -0300

    Criação da função de pesquisa dos contatos

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,11 +1,37 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
+from django.db.models import Q
 
 
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')
     context = {
-        'contacts': contacts
+        'contacts': contacts,
+        'site_title': 'Contatos - '
+    }
+
+    return render(
+        request, 'contact/index.html', context
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(email__icontains=search_value) |
+            Q(phone__icontains=search_value))\
+        .order_by('-id')
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - ',
+        'search_value': search_value,
     }
 
     return render(
@@ -16,9 +42,11 @@ def index(request):
 def contact(request, contact_id):
 
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
-    
+    contact_name = f'{single_contact.first_name} {single_contact.last_name} - '
+
     context = {
         'contact': single_contact,
+        'site_title': contact_name
     }
 
     return render(
