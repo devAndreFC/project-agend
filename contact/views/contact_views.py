3ff3f3500cf612e760afbc70e09855d0876dab74
Andre Oliveira <dev.andrefc@gmail.com>
2023-05-17 19:15:55 -0300

    Adicinado paginação no index e na pesquisa

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')
+    paginator = Paginator(contacts, 10)  
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
-
+    
     return render(
         request, 'contact/index.html', context
     )
@@ -28,8 +32,12 @@ def search(request):
             Q(email__icontains=search_value) |
             Q(phone__icontains=search_value))\
         .order_by('-id')
+    paginator = Paginator(contacts, 10)  
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Search - ',
         'search_value': search_value,
     }
