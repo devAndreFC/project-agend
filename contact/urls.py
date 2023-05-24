4d8a1730fb7dd345bf0aac24daf1c354a952bf5d
Andre Oliveira <dev.andrefc@gmail.com>
2023-05-23 21:54:34 -0300

    Criação da view, url e template para a exclusão do contato

--- contact/urls.py
+++ contact/urls.py
@@ -6,10 +6,8 @@ urlpatterns = [
     path('search/', views.search, name='search'),    
     path('<int:contact_id>/detail/', views.contact, name='contact'),
     path('<int:contact_id>/update/', views.update, name='update'),
-    #path('<int:contact_id>/delete/', views.delete, name='delete'),
+    path('<int:contact_id>/delete/', views.delete, name='delete'),
     path('contact/create/', views.create, name='create'),
 
-    
     path('', views.index, name='index'),
-
 ]
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -53,3 +53,18 @@ def update(request, contact_id):
     return render(
         request, 'contact/create.html', context
     )
+
+
+def delete(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+
+    confirmation = request.POST.get('confirmation', 'no')
+    print('confirmation:', confirmation)
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('contact:index')
+
+    return render(request, 'contact/contact.html', {
+        'confirmation': confirmation,
+        'contact': contact
+    })
